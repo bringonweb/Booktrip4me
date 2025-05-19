befe339557e187c687f8439485a8ff9aac992708
bringonweb <adityagupta@bringon.info>
2025-05-19 18:12:01 +0530

    fixed date pickers inside forms

--- index.php
+++ index.php
@@ -75,13 +75,13 @@
 
                 <div class="form-group" style="flex:1">
                   <!-- <label>Departure</label> -->
-                  <input class="form-control" placeholder="Departure on" type="text" onfocus="(this.type='date')"
-                    onblur="(this.type='text')">
+                  <input class="form-control" placeholder="Departure on" type="text"
+                    onfocus="(this.type='date', this.showPicker())" onblur="(this.type='text')">
                 </div>
                 <div class="form-group return-date" style="flex:1">
                   <!-- <label class="mm">Return</label> -->
-                  <input class="form-control" placeholder="Returning on" type="text" onfocus="(this.type='date')"
-                    onblur="(this.type='text')">
+                  <input class="form-control" placeholder="Returning on" type="text"
+                    onfocus="(this.type='date', this.showPicker())" onblur="(this.type='text')">
                 </div>
                 <div class="form-group" style="flex:1">
                   <!-- <label>Travellers</label> -->
@@ -106,13 +106,13 @@
               <div style="display:flex; flex-direction: row; justify-content: space-between; gap:5px ">
                 <div class="form-group" style="flex:1">
                   <!-- <label>Check-in</label> -->
-                  <input placeholder="Check In" type="text" onfocus="(this.type='date')" onblur="(this.type='text')"
-                    class="form-control">
+                  <input placeholder="Check In" type="text" onfocus="(this.type='date', this.showPicker())"
+                    onblur="(this.type='text')" class="form-control">
                 </div>
                 <div class="form-group" style="flex:1">
                   <!-- <label>Check-out</label> -->
-                  <input placeholder="Check Out" type="text" onfocus="(this.type='date')" onblur="(this.type='text')"
-                    class="form-control">
+                  <input placeholder="Check Out" type="text" onfocus="(this.type='date', this.showPicker())"
+                    onblur="(this.type='text')" class="form-control">
                 </div>
               </div>
               <div style="display:flex; flex-direction: row; justify-content: space-between; gap:5px ">
@@ -156,13 +156,13 @@
               <div style="display:flex; flex-direction: row; justify-content: space-between; gap:5px ">
                 <div class="form-group" style="flex:1;">
                   <!-- <label>Departure</label> -->
-                  <input placeholder="Departure on" type="text" onfocus="(this.type='date')" onblur="(this.type='text')"
-                    class="form-control">
+                  <input placeholder="Departure on" type="text" onfocus="(this.type='date', this.showPicker())"
+                    onblur="(this.type='text')" class="form-control">
                 </div>
                 <div class="form-group" style="flex:1;">
                   <!-- <label>Return Date</label> -->
-                  <input placeholder="Returning on" type="text" onfocus="(this.type='date')" onblur="(this.type='text')"
-                    class="form-control">
+                  <input placeholder="Returning on" type="text" onfocus="(this.type='date', this.showPicker())"
+                    onblur="(this.type='text')" class="form-control">
                 </div>
               </div>
               <div style="display:flex; flex-direction: row; justify-content: space-between; gap:5px ">
@@ -209,13 +209,13 @@
 
                 <div class="form-group" style="flex:1;">
                   <!-- <label>Trip Start</label> -->
-                  <input placeholder="Trip start" type="text" onfocus="(this.type='date')" onblur="(this.type='text')"
-                    class="form-control">
+                  <input placeholder="Trip start" type="text" onfocus="(this.type='date', this.showPicker())"
+                    onblur="(this.type='text')" class="form-control">
                 </div>
                 <div class="form-group" style="flex:1;">
                   <!-- <label>Trip End</label> -->
-                  <input placeholder="Trip End" type="text" onfocus="(this.type='date')" onblur="(this.type='text')"
-                    class="form-control">
+                  <input placeholder="Trip End" type="text" onfocus="(this.type='date', this.showPicker())"
+                    onblur="(this.type='text')" class="form-control">
                 </div>
 
                 <div class="form-group" style="flex:1;">
