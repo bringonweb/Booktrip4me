f758ca8c6b9cefc87508a5746729f49b80241334
bringonweb <adityagupta@bringon.info>
2025-05-20 18:39:33 +0530

    optimized image loading

--- main.js
+++ main.js
@@ -51,6 +51,13 @@ const tabImgs = {
   "cruises": 'CruiseShip.png',
   "packages": 'Car.png',
 };
+
+Object.values(tabImgs).forEach(src => {
+  const img = new Image();
+  img.src = `./assets/${src}`;
+});
+
+
 const heroBg = {
   "flights": 'https://cdn.pixabay.com/photo/2016/04/15/10/23/grindelwald-1330662_1280.jpg',
   "hotels": 'https://plus.unsplash.com/premium_photo-1661964298224-7747aa0ac10c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
